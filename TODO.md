ef37f7c32b4cfd800b72ec372473c6b099b5eb2f
AMwolf9000 <169104276+AMwolf9000@users.noreply.github.com>
2024-11-02 21:37:06 -0400

    Add files via upload

--- TODO.md
+++ TODO.md
@@ -1,7 +1,23 @@
 # Web Calculator
 This is a project to make a working calculator that is intuitive to use, looks good, and runs well
 
-### Baby steps
+### JS functionality
+ - [x] add exponent support
+ - [x] add decimal support
+ - [x] add negative number support
+ - [x] add input error correction (e.g. don't allow the input of double "+")
+ - [ ] allow calculator to follow PEMDAS
+
+### Frontend style
+ - [ ] calculator scales to screen (along with inner elements up to a max) 
+ - [ ] improve the overall look and feel of the calculator with user help 
+
+
+
+## Completed catagories
+
+### Baby steps 
+- Completed 10/29/2024
  - [x] add a front end input for users to interact with website
  - [x] enahnce front end with style to make it look appealing
  - [x] add basic calculator functionality with JS
--- index.js
+++ index.js
@@ -1,12 +1,42 @@
+"use strict";
 console.log("JS loaded!");
 
-const allButtons = Array.from(document.querySelectorAll("button"));
+const allButtons = [...document.querySelectorAll("button")];
 const display = document.querySelector("input");
 
-allButtons.forEach(function (e) {
+allButtons.forEach(function (e) { // adds button functionality
     if (e.className === "") {
         e.addEventListener("click", () => {
-            display.value += e.innerText;
+                let inputCheck = [display.value[display.value.length - 1], display.value[display.value.length - 2]]; // last two inputs
+                switch (e.innerText) { // check for iregular input
+                    case "+":
+                        if ( /-|\+|\*|\^|\/|\./.test(inputCheck[0]) || inputCheck[0] == undefined) return;
+                        break;
+                    case "*":
+                        if ( /-|\+|\*|\^|\/|\./.test(inputCheck[0]) || inputCheck[0] == undefined) return;
+                        break;
+                    case "/":
+                        if ( /-|\+|\*|\^|\/|\./.test(inputCheck[0]) || inputCheck[0] == undefined) return;
+                        break;
+                    case "^":
+                        if ( /-|\+|\*|\^|\/|\./.test(inputCheck[0]) || inputCheck[0] == undefined) return;
+                        break;
+                    case "-":
+                        if ( /-|\+|\*|\^|\/|\./.test(inputCheck[1]) && !/\d/.test(inputCheck[0]) || /-|\./.test(inputCheck[0]) && inputCheck[1] == undefined || inputCheck[0] == ".") return;
+                        break;
+                    case ".":
+                        let currentIndex = 0;
+                        display.value.matchAll(/-|\+|\*|\/|\^/ig).forEach((e) => {
+                            if (e.index > currentIndex) currentIndex = e.index;
+                        });
+                        
+                        let lastNum = display.value.slice(currentIndex);
+                        
+                        if (lastNum.includes(".")) return;
+                        break;
+                } 
+
+                display.value += e.innerText;
         })
     }
     if (e.className === "Remove") {
@@ -19,24 +49,39 @@ allButtons.forEach(function (e) {
             display.value = calcInput(display.value);
         })
     }
+    if (e.className === "CA") {
+        e.addEventListener("click", () => {
+            display.value = "";
+        })
+    }
 })
 
-function calcInput(rawInput) {
+function calcInput(rawInput) { // call for parse input and math calculations
     let values = [];
     let counter = 0;
     let output = "";
 
-    for (let i = 0; i < rawInput.length; i++) {
-        if (values[counter] === undefined) {values[counter] = ""}
-        if (!/\d/i.test(rawInput[i])) {
+    for (let i = 0; i < rawInput.length; i++) { // seperates input into an array where numbers and symbols are seperate (expects string as input)
+        if (values[counter] === undefined) values[counter] = ""; // prevents adding to undefined
+
+        if (!/-|\d|\./i.test(rawInput[i])) { // tests for symbols (excluding "-" which gets tested in the next if statement)
             counter++;
-            values[counter] = "";
+            values[counter] = rawInput[i];
+            counter++;
+            continue;
         }
+
+        if (rawInput[i] === "-" && /\d/.test(rawInput[i-1])) { // test for subtraction (exludes "-" if it is part of a negative number)
+            counter++;
+            values[counter] = rawInput[i];
+            counter++;
+            continue;
+        }
+
         values[counter] += rawInput[i];
-        if (!/\d/i.test(rawInput[i])) counter++;
     }
 
-    for (let i = 0; i < values.length; i++) {
+    for (let i = 0; i < values.length; i++) { // performs math calculations on given input (expects array as input)
         let n1 = Number(values[0]);
         let n2 = Number(values[2]);
         let symbol = values[1];
@@ -53,6 +98,9 @@ function calcInput(rawInput) {
                 break;
             case "/":
                 output = n1 / n2;
+                break;
+            case "^":
+                output = n1 ** n2;
                 break;
         }
         values.shift();
